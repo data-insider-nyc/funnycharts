60f17dcc86715d51f48e531ff7741d7621092309
Karl Kwon <newmkka@gmail.com>
2024-08-04 16:09:16 -0400

    all improvements

--- src/index.js
+++ src/index.js
@@ -3,32 +3,15 @@ var myChart = echarts.init(dom, null, {
   renderer: "canvas",
   useDirtyRect: false,
 });
-var app = {};
-
-// const waterMarkText = "Funny Charts";
-// const canvas = document.createElement("canvas");
-// const ctx = canvas.getContext("2d");
-// canvas.width = canvas.height = 100;
-// ctx.textAlign = "center";
-// ctx.textBaseline = "middle";
-// ctx.globalAlpha = 0.1;
-// ctx.font = "20px Microsoft Yahei";
-// ctx.translate(50, 50);
-// ctx.rotate(-Math.PI / 4);
-// ctx.fillText(waterMarkText, 0, 0);
-
 var option;
 
+var chart_title = "How a Golfer\nSpends Their Time";
 var data = [
   { value: 40, name: "Lost ball 😱" },
   { value: 25, name: "Complaining 🤬" },
   { value: 20, name: "Hitting the ball 🏌️‍♂️" },
   { value: 15, name: "Nature break 🌳" },
 ];
-
-
-var chart_title = "How a Golfer\nSpends Their Time";
-
 
 option = {
   title: {
@@ -40,13 +23,14 @@ option = {
     top: 100,
   },
   tooltip: {
+    show: false,
     trigger: "item",
   },
   toolbox: {
     show: true,
     feature: {
-      restore: {},
-      saveAsImage: {},
+      // restore: {},
+      // saveAsImage: {},
     },
   },
   series: [
@@ -69,9 +53,9 @@ option = {
       emphasis: {
         label: {
           show: true,
-          fontSize: 40,
-          fontWeight: 'bold'
-        }
+          fontSize: 50,
+          fontWeight: "bold",
+        },
       },
       labelLayout: function (params) {
         const isLeft = params.labelRect.x < myChart.getWidth() / 2;
@@ -89,8 +73,44 @@ option = {
   ],
 };
 
-if (option && typeof option === "object") {
-  myChart.setOption(option);
+// Set the options to the chart
+myChart.setOption(option);
+
+// Resize the chart on window resize
+window.addEventListener("resize", function () {
+  myChart.resize();
+});
+
+function highlightLoop() {
+  let currentIndex = -1;
+  const dataLen = option.series[0].data.length;
+
+  setInterval(() => {
+    // Cancel highlight of previous slice
+    myChart.dispatchAction({
+      type: "downplay",
+      seriesIndex: 0,
+      dataIndex: currentIndex,
+    });
+
+    // Move to the next slice
+    currentIndex = (currentIndex + 1) % dataLen;
+
+    // Highlight the current slice
+    myChart.dispatchAction({
+      type: "highlight",
+      seriesIndex: 0,
+      dataIndex: currentIndex,
+    });
+
+    // Optionally, display tooltip for the current slice
+    myChart.dispatchAction({
+      type: "showTip",
+      seriesIndex: 0,
+      dataIndex: currentIndex,
+    });
+  }, 1000); // Change slice every 2 seconds
 }
 
-window.addEventListener("resize", myChart.resize);
+// Start the highlight loop
+highlightLoop();
